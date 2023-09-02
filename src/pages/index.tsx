9e57b62e042b17edc8e7034faeb309f54263563d
psyfer <psyferblock.io@gmail.com>
2023-09-02 13:35:28 +0300

    added clerk auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 import { api } from "~/utils/api";
 import styles from "./index.module.css";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+const user=useUser()
   return (
+    
     <>
       <Head>
         <title>Create T3 App</title>
@@ -16,36 +18,10 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.container}>
-          <h1 className={styles.title}>
-            Create <span className={styles.pinkSpan}>T3</span> App
-          </h1>
-          <div className={styles.cardRow}>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
-              <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <p className={styles.showcaseText}>
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+          {!user.isSignedIn &&  <SignInButton/>}
+          {!!user.isSignedIn && <SignOutButton/>}
+        
+         
         </div>
       </main>
     </>
